a9373f0e24788939865c86489acc7e25c258482c
Alex Flores <SorAxel360@hotmail.com>
2019-01-26 20:39:47 -0500

    + Added Camera scrip
    + Added script for the camera to move
    + added multiple camera positions

---
+++ GameEngineProject/Assets/MoveCameraPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraPosition : MonoBehaviour
+{
+    private Camera camera;
+
+    [SerializeField] private GameObject cameraNewPosition;
+
+    private void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            camera.transform.position = cameraNewPosition.transform.position;
+            camera.transform.rotation = cameraNewPosition.transform.rotation;
+        }
+    }
+}
--- GameEngineProject/Assets/TankControls.cs
+++ GameEngineProject/Assets/TankControls.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [AddComponentMenu("Camera-Control/Mouse Look")]
 public class TankControls : MonoBehaviour
 {
+    private float rotationX;
+
     [SerializeField] private enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     [SerializeField] RotationAxes axes = RotationAxes.MouseXAndY;
 
@@ -21,16 +23,23 @@ public class TankControls : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetButton("Horizontal"))
+        {
+            rotationX = transform.localEulerAngles.y + Input.GetAxis("Horizontal") * sensitivityX;
+
+        }
+
+
         if (axes == RotationAxes.MouseXAndY)
         {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Horizontal") * sensitivityX;
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 
             transform.Translate(0f, 0f, walkingSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
 
             transform.localEulerAngles = new Vector3(0, rotationX, 0);
         }
         else if (axes == RotationAxes.MouseX)
-            transform.Rotate(0, Input.GetAxis("Horizontal") * sensitivityX, 0 );
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0 );
         else
         {
             transform.Translate(0f, 0f, walkingSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
