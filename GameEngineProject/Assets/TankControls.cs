22a1f486dfb656a9e832412c39483b21a2669994
Alex Flores <SorAxel360@hotmail.com>
2019-01-28 16:00:45 -0500

    * Updated to unity 2018.3.3
    * Updated unity

--- GameEngineProject/Assets/TankControls.cs
+++ GameEngineProject/Assets/TankControls.cs
@@ -2,17 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[AddComponentMenu("Camera-Control/Mouse Look")]
 public class TankControls : MonoBehaviour
 {
     private float rotationX;
 
-    [SerializeField] private enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-    [SerializeField] RotationAxes axes = RotationAxes.MouseXAndY;
-
     [SerializeField] float sensitivityX = 15f;
-    [SerializeField] float minimumX = -360f;
-    [SerializeField] float maximumX = 360f;
     [SerializeField] private float walkingSpeed = 1f;
 
     void Start()
@@ -26,25 +20,13 @@ public class TankControls : MonoBehaviour
         if (Input.GetButton("Horizontal"))
         {
             rotationX = transform.localEulerAngles.y + Input.GetAxis("Horizontal") * sensitivityX;
-
-        }
-
-
-        if (axes == RotationAxes.MouseXAndY)
-        {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-
-            transform.Translate(0f, 0f, walkingSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
-
             transform.localEulerAngles = new Vector3(0, rotationX, 0);
-        }
-        else if (axes == RotationAxes.MouseX)
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0 );
-        else
-        {
-            transform.Translate(0f, 0f, walkingSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
-            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
-        }
+            transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
 
+        }
+        else if (Input.GetAxis("Mouse X") > 0.1 || Input.GetAxis("Mouse X") < -0.1)
+            transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * sensitivityX , 0);
+
+        transform.Translate(0f, 0f, walkingSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
     }
 }
