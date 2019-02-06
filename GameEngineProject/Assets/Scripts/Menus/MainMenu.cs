eb38c721d7c1f57ee44bb21c7d017852aef43fe3
Alex Flores <SorAxel360@hotmail.com>
2019-02-06 12:12:35 -0500

    + Main Menu and Option menu
    + Added More to Main menu
    + Added Option menu
    + Added Fonts

---
+++ GameEngineProject/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject[] panels;
+    [SerializeField] private Selectable[] defaultOptions;
+
+    public void PanelToggle(int position)
+    {
+        Input.ResetInputAxes();
+
+        for (int i = 0; i < panels.Length; i++)
+        {
+            panels[i].SetActive(position == i);
+
+            if (position == i)
+                defaultOptions[i].Select();
+        }
+    }
+}
---
+++ GameEngineProject/Assets/Scripts/Menus/SelectableObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SelectableObject : MonoBehaviour, IPointerEnterHandler, IDeselectHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponent<Selectable>().Select();
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        GetComponent<Selectable>().OnPointerExit(null);
+    }
+}
