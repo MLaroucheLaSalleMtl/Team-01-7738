1201947b24ec4dd88df84f5a994bb556923f0354
Raihi <ibakaq.q@gmail.com>
2019-02-27 13:48:31 -0500

    +AddedMike
    New character

--- GameEngineProject/Assets/Character/New Folder/CharMove.cs
+++ GameEngineProject/Assets/Character/New Folder/CharMove.cs
@@ -51,7 +51,7 @@ public class CharMove : MonoBehaviour
         }
 
         anim.SetFloat("v", Input.GetAxis("Vertical"));
-        anim.SetFloat("h", Input.GetAxis("Horizontal"));
+        //anim.SetFloat("h", Input.GetAxis("Horizontal"));
         if (Input.GetButton("Fire3") && Input.GetButton("Vertical"))
         {
             anim.SetBool("Running", true);
@@ -60,15 +60,15 @@ public class CharMove : MonoBehaviour
             anim.SetBool("Running", false);
         moveDirection = Vector3.zero;
 
-        //if (Input.GetButton("Horizontal") && Input.GetAxis("Vertical") > 0)
-        //{
-        //    rotationX = transform.localEulerAngles.y + Input.GetAxis("Horizontal") * sensitivityX;
-        //    transform.localEulerAngles = new Vector3(0, rotationX, 0);
-        //    transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
-        //}
+        if (Input.GetButton("Horizontal") && Input.GetAxis("Vertical") > 0)
+        {
+            rotationX = transform.localEulerAngles.y + Input.GetAxis("Horizontal") * sensitivityX;
+            transform.localEulerAngles = new Vector3(0, rotationX, 0);
+            transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+        }
         //else if (Input.GetAxis("Mouse X") > 0.1 || Input.GetAxis("Mouse X") < -0.1)
         //    transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
 
-        //transform.Translate(0f, 0f, walkingSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        transform.Translate(0f, 0f, walkingSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
     }
 }
