6d83d539514558254c5e4e27c9829b54d8305432
Alex Flores <SorAxel360@hotmail.com>
2019-05-03 01:05:42 -0400

    +Added Ending
    +Added Ending

--- GameEngineProject/Assets/Scripts/Game/NoteManager.cs
+++ GameEngineProject/Assets/Scripts/Game/NoteManager.cs
@@ -6,10 +6,21 @@ public class NoteManager : MonoBehaviour
 {
     private List<GameObject> notes;
 
+    private bool hasFiveNote = false;
+
     public List<GameObject> Notes { get => notes; set => notes = value; }
+    public bool HasFiveNote { get => hasFiveNote; set => hasFiveNote = value; }
 
     void Start()
     {
         notes = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        if (notes.Count == 5)
+        {
+            hasFiveNote = true;
+        }    
     }
 }
---
+++ GameEngineProject/Assets/Scripts/Interactables/Keys and Doors/MainHospitalDoor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainHospitalDoor : Doors
+{
+    private NoteManager noteManager;
+    private AsyncOperation async;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        noteManager = FindObjectOfType<NoteManager>();
+    }
+
+
+    protected override void Interact()
+    {
+        if (noteManager.HasFiveNote)
+        {
+            StartCoroutine(DisplayText(interactedText));
+            Invoke("SecondInteract", 3f);
+
+        }
+        else
+            StartCoroutine(DisplayText("..."));
+    }
+
+    protected override void SecondInteract()
+    {
+        OpenMainDoor();
+    }
+
+    void OpenMainDoor()
+    {
+        animator.SetBool("FadeIn/Out", true);
+        Scene currentScene = SceneManager.GetActiveScene();
+        async = SceneManager.LoadSceneAsync(0);
+        async.allowSceneActivation = true;
+    }
+
+    protected override IEnumerator OpenDoor()
+    {
+        yield return new WaitForSeconds(0);
+    }
+}
